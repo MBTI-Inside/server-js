e92cdb6a29ed46c4c8a6c0f00aa0be058adb67cb
rebi13 <rebi13@naver.com>
2024-06-29 01:45:40 +0900

    feature: buildResponse 유틸 제거 (데이터 형식 맞추기 위함)

--- src/middlewares/asyncHandler.js
+++ src/middlewares/asyncHandler.js
@@ -1,10 +1,11 @@
-import { buildResponse } from '../misc/utils.js';
+// import { buildResponse } from '../misc/utils.js';
 
 const asyncHandler = (requestHandler) => {
   return async (req, res, next) => {
     try {
-      const response = await requestHandler(req, res); // 수정된 부분
-      return res.json(buildResponse(response));
+      const response = await requestHandler(req, res);
+      return res.json(response); // @MCprotein 데이터 형식 맞추기 위해 buildResponse 유틸 함수 미사용
+      // return res.json(buildResponse(response));
     } catch (err) {
       next(err);
     }
