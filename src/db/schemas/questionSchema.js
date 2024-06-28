876eded929561af1ae158003e283b5157a44ab55
rebi13 <rebi13@naver.com>
2024-06-29 01:55:49 +0900

    refactor: 서브스키마(answer) 분리

--- src/db/schemas/questionSchema.js
+++ src/db/schemas/questionSchema.js
@@ -1,28 +1,5 @@
 import { Schema } from 'mongoose';
-
-// Answer Schema
-const answerSchema = new Schema(
-  {
-    type: {
-      type: String,
-      required: true,
-      enum: ['I', 'E', 'S', 'N', 'T', 'F', 'J', 'P'] // MBTI 타입
-    },
-    content: {
-      type: String,
-      required: true
-    },
-    proportion: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 100
-    }
-  },
-  {
-    _id: false // 이 서브 스키마에 대해 id 생성 안 함
-  }
-);
+import answerSchema from './answerSchema';
 
 // Question Schema
 const questionSchema = new Schema(
