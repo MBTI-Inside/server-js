b0537066dda90ea643ac9247f56ed5039a5a8070
rebi13 <rebi13@naver.com>
2024-06-29 18:36:02 +0900

    feature: DB 반환 데이터에서 password 제외 MBTI-Inside/todo#2

--- src/db/models/memoModel.js
+++ src/db/models/memoModel.js
@@ -24,20 +24,29 @@ class MemoModel {
 
   // 게시글 document 객체 전체를 찾아오는 메소드
   findMemos() {
-    return Memo.find().lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
+    return Memo.find().select('-password').lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
   }
 
   // 특정 id를 _id로 갖는 게시글 document 객체를 찾아오는 메소드
   findById(id) {
     return Memo.findById(
       id // document의 id, mongoDB에 저장된 _id에 저장된 값이다.
-    ).lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
+    )
+      .select('-password')
+      .lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
   }
 
   // 새로운 게시글 document 객체를 생성하여 mongoDB에 저장하는 메소드
   create(memo) {
     // 생성된 객체는 값만 있는 non-POJO 객체이다. toObject를 이용해서 POJO 객체로 바꿔준다.
-    return Memo.create(memo).toObject();
+    return Memo.create(memo).then((doc) =>
+      doc.toObject({
+        transform: (doc, ret) => {
+          delete ret.password;
+          return ret;
+        }
+      })
+    ); // password 필드를 제외하고 반환
   }
 
   // 특정 id를 _id로 갖고 있는 게시글 document를 toUpdate 객체의 내용으로 덮어 씌운다(overwrite).
@@ -50,7 +59,9 @@ class MemoModel {
         runValidators: true, // schema 체크(업데이트 될 데이터에 대한 검증)를 진행한다.
         new: true // 업데이트 후의 document를 리턴받도록 한다.
       }
-    ).lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
+    )
+      .select('-password')
+      .lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
     return updatedMemo;
   }
 
@@ -67,7 +78,9 @@ class MemoModel {
   delete(id) {
     return Memo.findByIdAndDelete(
       id // document의 id, mongoDB에 저장된 _id에 저장된 값이다.
-    ).lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
+    )
+      .select('-password')
+      .lean(); // lean을 사용하여 POJO 객체로 바꿔준다.
   }
 }
 
