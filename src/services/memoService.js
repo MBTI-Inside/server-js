bde398de47ac28edf01955a5c2c8981fffdc6df1
rebi13 <rebi13@naver.com>
2024-06-29 18:35:38 +0900

    chore: TODO comment 추가 MBTi-Inside/todo#2

--- src/services/memoService.js
+++ src/services/memoService.js
@@ -9,6 +9,8 @@ class MemoService {
     return this.memoModel.findById(id);
   }
   getMemos() {
+    // TODO: 댓글 수는 해당 메모 Id에 대해 댓글 수를 조회하여 count 값을 가져오는 것이 아니라
+    // 댓글을 작성할 때 해당 메모 Id에 대해 댓글 수를 업데이트 하는 방식으로 구현해야 한다.
     return this.memoModel.findMemos();
   }
   addMemo(memo) {
