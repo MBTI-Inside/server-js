11edbe22084c3ee676dcae70d8900700181256a2
rebi13 <rebi13@naver.com>
2024-06-30 02:22:56 +0900

    feature: MBTI 스키마에 필드 추가 (summary, tags, fit) MBTI-Inside/todo#3

--- src/db/schemas/mbtiSchema.js
+++ src/db/schemas/mbtiSchema.js
@@ -1,7 +1,9 @@
 import { Schema } from 'mongoose';
+import fitSchema from './fitSchema';
 
 const mbtiSchema = new Schema(
   {
+    // MBTI 16가지 유형
     type: {
       type: String,
       required: true,
@@ -25,6 +27,28 @@ const mbtiSchema = new Schema(
       ],
       unique: true
     },
+    // MBTI 요약 (해당 MBTI를 한 마디로 표현)
+    summary: {
+      type: String,
+      required: true
+    },
+    // 해당 MBTI 주요 키워드
+    tags: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (v) {
+          return Array.isArray(v) && v.length === 4;
+        },
+        message: (props) => `${props.value} should have exactly 4 elements`
+      }
+    },
+    // 해당 MBTI와 어울림
+    fit: {
+      type: [fitSchema], // fit은 fitSchema 타입의 객체 배열 (good, bad)
+      required: true
+    },
+    // 해당 MBTI 유형 결과 수치
     count: {
       type: Number,
       required: true,
