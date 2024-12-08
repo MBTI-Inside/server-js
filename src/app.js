51b2e08b1d311abdd0915e470be33f0caf5f9067
rebi13 <rebi13@naver.com>
2024-12-08 23:05:50 +0900

    CORS URL 적용

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import commonErrors from './misc/commonErrors.js';
 import { v1 } from './routers/index.js';
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.ORIGIN_URL ?? 'http://localhost:5173',
   optionsSuccessStatus: 200,
   credentials: true
 };
